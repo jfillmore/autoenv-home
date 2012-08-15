dfb949864951bcf2c763608e244ae59ef068e263
Jonny Fillmore <jonny@uk2group.com>
2012-08-15 08:51:07 -0700

    bugfix on base_dir var name

--- index_all.sh
+++ index_all.sh
@@ -8,7 +8,7 @@ fail() {
 base_dir=$(cd $(dirname "$0") && pwd -P)
 auto_env_opts="--verbose"
 
-cd "$BASE_DIR" || fail "Failed to change dir to '$BASE_DIR'."
+cd "$base_dir" || fail "Failed to change dir to '$base_dir'."
 find -maxdepth 1 -type d -not -name \.\* -print0 \
     | xargs -0 auto_env/bin/auto_env.sh -i $auto_env_opts \
     || fail "Failed to generate index files."

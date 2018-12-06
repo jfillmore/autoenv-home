9d15fcf859dbd3abbbe0d2a54a8f912858f4d16d
Jonny Fillmore <jonny@spamriot.com>
2018-12-06 10:54:56 -0800

    smarter stats.py, only do extra init bash init scripts on interactive terms

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,18 +1,22 @@
 # .bashrc
 
+# always load some common defaults
 if [ -f /etc/bashrc ]; then
     . /etc/bashrc
 fi
-
 if [ -f "$HOME/.bashrc.local" ]; then
     . "$HOME/.bashrc.local"
 fi
-if [ -d "$HOME/.bashrc.d/" ]; then
-    for __file in "$HOME"/.bashrc.d/*; do
-        # if no files exist this returns the un-globbed string
-        # also, just ignore empty stuff
-        if [ -s "$__file" ]; then
-            . "$__file"
-        fi
-    done
+
+# only load the extras if we're an interactive shell
+if [[ "$-" =~ 'i' ]]; then
+    if [ -d "$HOME/.bashrc.d/" ]; then
+        for __file in "$HOME"/.bashrc.d/*; do
+            # if no files exist this returns the un-globbed string
+            # also, just ignore empty stuff
+            if [ -s "$__file" ]; then
+                . "$__file"
+            fi
+        done
+    fi
 fi
--- bash/scripts/stats.py
+++ bash/scripts/stats.py
@@ -24,6 +24,8 @@ def sd_calc(data):
 def avg_calc(ls):  
     n, mean = len(ls), 0.0  
   
+    if n == 0:
+        return 0
     if n <= 1:  
         return ls[0]  
   
@@ -31,40 +33,122 @@ def avg_calc(ls):
         mean = mean + float(el)  
     mean = mean / float(n)  
   
-    return mean  
+    return mean
 
 
-def main():
-    nums = []
-    skipped = 0
-    filtered = 0
-    num_re = re.compile('[^-.0-9]')
-    for line in sys.stdin.readlines():
-        try:
-            nums.append(float(line))
-        except Exception as e:
-            try:
-                nums.append(float(
-                    ''.join(filter(
-                        lambda a: a if a else None,
-                        re.split('[^-.0-9]', line)
-                    ))
-                ))
-                filtered += 1
-            except Exception as e:
-                skipped += 1
-                continue
-    print "count: %d (skipped: %d, filtered %d), avg: %.2f, stddev: %.2f, range: %s - %s, sum: %.2f" % (
+def print_stats(col_num, headers, nums, skipped, filtered):
+    sys.stdout.write('%d. ' % (col_num + 1))
+    if headers:
+        sys.stdout.write('%s = ' % (headers[col_num]))
+    sys.stdout.write("count: %d, avg: %.2f, stddev: %.2f, range: %s - %s, sum: %.2f (skipped: %s, filtered %s)\n" % (
         len(nums),
-        skipped,
-        filtered,
         avg_calc(nums),
         sd_calc(nums),
-        min(nums),
-        max(nums),
-        sum(nums)
-    )
+        min(nums) if nums else None,
+        max(nums) if nums else None,
+        sum(nums),
+        skipped,
+        filtered,
+    ))
+
+
+def main(dedupe_col=None, ifs_re='\s+', has_headers=False, to_skip='', dedupe_file=None):
+    headers = []
+    grid = dupes_grid = skipped = filtered = None  # all to become arrays based on # of columns
+    if dedupe_col is not None:
+        dedupe_col = int(dedupe_col) - 1
+    seen = set()
+    num_cols = None
+    if to_skip:
+        to_skip = map(int, to_skip.split(','))
+    else:
+        to_skip = []
+
+    rows = 0
+    if dedupe_file:
+        if not dedupe_col:
+            raise Exception("You must specify a column to key de-duplication to provide a dedupe_file")
+        fh_uniq = open(dedupe_file + '.uniq', 'w')
+        fh_dupes = open(dedupe_file + '.dupes', 'w')
+
+    for line in sys.stdin.readlines():
+        line = line.strip()
+        if ifs_re:
+            line_parts = re.split(ifs_re, line, 0 if not num_cols else num_cols - 1)
+        else:
+            line_parts = [line]
+        if grid is None:
+            num_cols = len(line_parts)
+            grid = []
+            dupes_grid = []
+            for i in range(0, num_cols):
+                grid.append([])
+                dupes_grid.append([])
+            skipped = [0] * num_cols
+            filtered = [0] * num_cols
+        if bool(int(has_headers)):
+            headers = line_parts
+            has_headers = False
+            if dedupe_file:
+                fh_uniq.write(line + '\n')
+                fh_dupes.write(line + '\n')
+            continue
+        dedupe_key = line_parts[dedupe_col] if dedupe_col is not None else None
+
+        # convert to numerical values where we can
+        nums = []
+        for i, num in enumerate(line_parts):
+            try:
+                nums.append(float(num))
+            except Exception as e:
+                try:
+                    nums.append(float(
+                        ''.join(filter(
+                            lambda a: a if a else None,
+                            re.split('[^-.0-9]', line)
+                        ))
+                    ))
+                    filtered[i] += 1
+                except Exception as e:
+                    nums.append(None)
+                    skipped[i] += 1
+                    continue
+        # dedupe if needed, otherwise track in the grid
+        if dedupe_key is not None and dedupe_key in seen:
+            for i, num in enumerate(nums):
+                if num is not None and i not in to_skip:
+                    dupes_grid[i].append(num)
+            if dedupe_file:
+                fh_dupes.write(line + '\n')
+        else:
+            if dedupe_key is not None:
+                seen.add(dedupe_key)
+            for i, num in enumerate(nums):
+                if num is not None and i not in to_skip:
+                    grid[i].append(num)
+            if dedupe_file:
+                fh_uniq.write(line + '\n')
+        rows += 1
+
+    # and print our report
+    for i, nums in enumerate(grid):
+        print_stats(i, headers, nums, skipped[i], filtered[i])
+    if dedupe_col:
+        sys.stdout.write("\n--- DUPES ---\n")
+        for i, nums in enumerate(dupes_grid):
+            print_stats(i, headers, nums, skipped[i], filtered[i])
+    if dedupe_file:
+        fh_uniq.close()
+        fh_dupes.close()
+        sys.stdout.write("\n- wrote %s.uniq and %s.dupes\n" % (
+            dedupe_file, dedupe_file
+        ))
+
 
 
 if __name__ == '__main__':
-    main()
+    args = {}
+    while len(sys.argv) > 1:
+        arg = sys.argv.pop().split('=')
+        args[arg[0]] = arg[1]
+    main(**args)

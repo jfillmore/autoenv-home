279ae193c4b1e6775dfbdd7232d8188d9b00bcd4
Jonny Fillmore <me@jkf.im>
2021-04-03 07:53:54 -0700

    dates in repeat

--- bash/scripts/repeat.sh
+++ bash/scripts/repeat.sh
@@ -48,14 +48,17 @@ esac
 line_break='────────────────────────────────────────────────────────────────────────────────'
 line_break="$line_break$line_break$line_break"
 
-# print initial start marker
+# print initial start header
 cols=$(tput cols)
-echo -e "\033[1;37m╓${line_break:0:cols-1}\033[1D╖\033[0m\n"
+echo -e "\033[1;37m╓${line_break:0:cols-1}\033[1D╖\033[0m"
+echo -e "\033[1A\033[3C \033[1;37m$(date)\033[0m \n"
 
 # loop forever-ish!
 while true; do
     # run command and prep output
+    time_start=$(date +%s)
     "$@"
+    time_end=$(date +%s)
     retval=$?
     [ $retval -eq 0 -a $repeat_fail_only -eq 1 ] && exit
     if [ $retval -eq 0 ]; then
@@ -68,13 +71,20 @@ while true; do
         color_line='0;31'
     fi
 
+    # print footer + prompt
     cols=$(tput cols)
     echo -e "\n\033[${color_line}m╙${line_break:0:cols-1}\033[1D╜\033[0m"
+    echo -e "\033[1A\033[3C \033[1;32m$(date)\033[0;32m [$((time_end - time_start)) s]\033[0m "
     echo -en " \033[${color_msg}m  -- $msg -- \033[0m"
     echo -ne "\033[${color_line}m│\033[1;37m  (\033[1;33m\\\n\033[0;37m to repeat, \033[1;33m^c\033[0;37m to quit) "
+
     # pause, hiding input so newlines don't cause goofy stuff
     stty -echo &>/dev/null
     read
     stty echo &>/dev/null
-    echo -e "\n\n\033[1;37m╓${line_break:0:cols-1}\033[1D╖\033[0m\n"
+
+    # print next header
+    cols=$(tput cols)
+    echo -e "\n\n\n\033[1;37m╓${line_break:0:cols-1}\033[1D╖\033[0m"
+    echo -e "\033[1A\033[3C \033[1;37m$(date)\033[0m \n"
 done

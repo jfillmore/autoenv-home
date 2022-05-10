f3f07ef828f410a9cd5ea0f66c5151b9b56642af
jkf <git@spamriot.com>
2022-05-09 17:37:10 -0700

    good stuff

--- bash/.bashrc.d/jkf.sh
+++ bash/.bashrc.d/jkf.sh
@@ -4,8 +4,14 @@ function setPrompt {
 
 function grim {
     local query="$1"; shift
-    vim "$@" -c "/$query" $(grep -R --binary-files=without-match "$query" . | awk -F: '{sub(/ /, "\\ "); print $1}' | sort -u)
+    local path="${1:-.}"; shift
+    IFS=$'\n' vim "$@" -c "/$query" $(
+        grep -R --binary-files=without-match "$query" "$path" \
+        | awk -F: '{print $1}' \
+        | sort -u
+    )
 }
+
 
 
 [ $(uname) = 'Darwin' ] && {
